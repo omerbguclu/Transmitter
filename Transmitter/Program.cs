b8e00555aa89878e0a18bf42e6a395aa16df99ac
omerbguclu <omerbguclu@gmail.com>
2020-02-26 16:15:24 +0300

    Initial commit

---
+++ Transmitter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace Transmitter
+{
+    class Program
+    {
+        public static void Main()
+        {
+            // using (var responder = new ResponseSocket())
+            // {
+            //     responder.Bind("tcp://*:5555");
+
+            //     while (true)
+            //     {
+            //         string str = responder.ReceiveFrameString();
+            //         Console.WriteLine(str);
+            //         Thread.Sleep(50);  //  Do some 'work'
+            //         responder.SendFrame("World");
+            //     }
+            // }
+            Console.WriteLine("Connecting to hello world server…");
+            using (var requester = new SubscriberSocket())
+            {
+                requester.Connect("tcp://localhost:5555");
+                requester.Subscribe("PUB");
+                int requestNumber = 0;
+                while (true)
+                {
+                    NetMQMessage arr = requester.ReceiveMultipartMessage();
+                    string str = arr[1].ConvertToString();
+                    Console.WriteLine(str);
+                    Console.WriteLine(requestNumber++);
+                }
+
+            }
+        }
+    }
+}
